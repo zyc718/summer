aa6159a02455352244319b45824c73b1893193f0
yuchuan3 <yuchuan3@staff.sina.com.cn>
2023-06-04 17:10:35 +0800

    更改port

--- route.go
+++ route.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func route() {
 	r := gin.Default()
 	apiCommon(r)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":8090") // listen and serve on 0.0.0.0:8080
 }
 
 func apiCommon(r *gin.Engine) {
